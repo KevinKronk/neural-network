3ea202d0bb515e34a03e4f63240a9cbb0211ced8
Kevin Kronk <kjkronk@ncsu.edu>
2019-03-31 13:02:06 -0400

    Formatted Code.

--- neural-network/neural_network.py
+++ neural-network/neural_network.py
@@ -1,14 +1,10 @@
-from scipy.io import loadmat
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.special import expit
-from scipy import optimize as opt
 from load_data import load_data
 from load_weights import load_weights
-from cost_function import cost_function
 from map_outputs import map_outputs
-from additional_funtions import sig_gradient
 from additional_funtions import rand_weight
+from train import train
 
 
 # Load Data and Weights
@@ -18,6 +14,35 @@ x, y = load_data(filename)
 
 weight_file = 'ex4weights.mat'
 params = load_weights(weight_file)
+
+
+# Set values and Load Weights
+
+input_layer = 400
+hidden_layer = 25
+output_layer = 10
+
+
+# Map Outputs
+
+k = 10
+y_map = map_outputs(y, k)
+
+
+# Initialize Randomized Parameters
+
+init_theta1 = rand_weight(input_layer, hidden_layer)
+init_theta2 = rand_weight(hidden_layer, output_layer)
+init_params = np.concatenate([init_theta1.ravel(), init_theta2.ravel()], axis=0)
+hyper_p = 10
+iters = 150
+
+
+# Train and Predict
+
+accuracy = train(init_params, x, y, y_map, hyper_p, iters)
+
+print(f"With a hyperparameter of {hyper_p}\nThe accuracy is: {accuracy}%")
 
 
 # Visualize the Images
@@ -41,43 +66,4 @@ for j in range(nrows):
         ax[j][i].imshow(create_image(images[c, :]), cmap='gray')
         ax[j][i].axis('off')
         c += 1
-# plt.show()
-
-
-# Set values and Load Weights
-
-input_layer = 400
-hidden_layer = 25
-output_layer = 10
-
-
-# Map Outputs
-
-k = 10
-y_map = map_outputs(y, k)
-
-
-cost = cost_function(params, x, y_map, hyper_p=0)
-
-print(cost)
-
-
-init_theta1 = rand_weight(input_layer, hidden_layer)
-init_theta2 = rand_weight(hidden_layer, output_layer)
-init_params = np.concatenate([init_theta1.ravel(), init_theta2.ravel()], axis=0)
-
-
-# result = back_propagation(init_thetas, x, y_map)
-# print(result)
-
-
-
-
-hyper_p = 10
-
-params = train(init_params, x, y_map, hyper_p, 150)
-theta1, theta2 = unroll_params(params)
-prediction = np.argmax(feed_forward([theta1, theta2], x)[1][1], axis=0)
-
-accuracy = np.mean(prediction == y.T) * 100
-print(f"With a hyperparameter of {hyper_p}\nThe accuracy is: {accuracy}%")
+plt.show()
--- neural-network/train.py
+++ neural-network/train.py
@@ -1,9 +1,16 @@
+import numpy as np
 from cost_function import cost_function
 from back_propagation import back_propagation
 from scipy import optimize as opt
+from cost_function import unroll_params
+from feed_forward import feed_forward
 
 
-def train(init_params, x, y_map, hyper_p=0, iters=100):
+def train(init_params, x, y, y_map, hyper_p=0, iters=100):
     result = opt.minimize(cost_function, init_params, args=(x, y_map, hyper_p),
                           jac=back_propagation, method='TNC', options={'maxiter': iters})
-    return result.x
+    params = result.x
+    theta1, theta2 = unroll_params(params)
+    prediction = np.argmax(feed_forward([theta1, theta2], x)[1][1], axis=0)
+    accuracy = np.mean(prediction == y.T) * 100
+    return accuracy
